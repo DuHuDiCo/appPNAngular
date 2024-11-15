a3004ecbbed68e2e96369bacbbd347714ca7ff3d
Tibiie <yuraniecheverri8@gmail.com>
2024-11-15 10:37:03 -0500

    Primeras vistas

--- pn-frontend/src/app/app.component.ts
+++ pn-frontend/src/app/app.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'pn-frontend';
 
+  constructor(private router: Router) {}
+
   ngOnInit(): void {
     initFlowbite();
+
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        initFlowbite();
+      }
+    });
   }
 }
